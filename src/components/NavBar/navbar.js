1ee22c366556c11dc1222fa9744e0ceb9c6356a1
Tassawar hasib <nabilhasib840@gmail.com>
2024-01-07 21:51:28 +0530

    Hamburger menu for mobile completed

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import contactImg from '../../assets/contact.png';
 import {Link} from 'react-scroll';
+import menu from '../../assets/menu.png';
 
 
 const Navbar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
         <img src={logo} alt="logo" className="logo"/>
@@ -22,6 +26,16 @@ const Navbar = () => {
             <img src={contactImg} alt="" className="desktopMenuImg"/>
             Contact Me
         </button>
+
+        <img src={menu} alt="Menu" className="mobMenu" onClick={()=>setShowMenu(!showMenu)} />
+        <div className="navMenu" style={{display: showMenu ? 'flex':'none'}}>
+            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem"  onClick={()=>setShowMenu(false)} >Home</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-80} duration={500} className="listItem" onClick={()=>setShowMenu(false)} >About</Link>
+            <Link activeClass='active' to='works' spy={true} smooth={true} offset={-70} duration={500} className="listItem" onClick={()=>setShowMenu(false)} >Portfolio</Link>
+            <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-40} duration={500} className="listItem" onClick={()=>setShowMenu(false)} >Clients</Link>
+            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-110} duration={500} className="listItem" onClick={()=>setShowMenu(false)} >Contact</Link>
+        </div>
+
 
     </nav>
   )
